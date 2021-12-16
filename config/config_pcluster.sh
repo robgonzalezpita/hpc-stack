f5f0fe8019f77986f2fd619d708bdcfabc0da299
Rob Gonzalez-Pita <rob.gonzalez-pita@noaa.gov>
2021-12-16 09:21:04 -0700

    Point to PCluster default Intel MPI
    Change HPC_MPI variable to the intel MPI shipped with PCluster v 2.11, clean up whitespace

--- config/config_pcluster.sh
+++ config/config_pcluster.sh
@@ -3,7 +3,7 @@
 # Compiler/MPI combination
 python_ver=$( python3 --version | cut -d " " -f2 | cut -d. -f1-2 )
 export HPC_COMPILER=${HPC_COMPILER:-"intel/2021"}
-export HPC_MPI=${HPC_MPI:-"impi/2021"}
+export HPC_MPI=${HPC_MPI:-"impi/2019.8.254"}
 export HPC_PYTHON=${HPC_PYTHON:-"python/${python_ver}"}
 
 # Build options
@@ -12,9 +12,9 @@ export PKGDIR=pkg
 export LOGDIR=log
 export OVERWRITE=N
 export NTHREADS=4
-export   MAKE_CHECK=N
+export MAKE_CHECK=N
 export MAKE_VERBOSE=N
-export   MAKE_CLEAN=N
+export MAKE_CLEAN=N
 export DOWNLOAD_ONLY=N
 export STACK_EXIT_ON_FAIL=Y
 export WGET="wget -nv"
